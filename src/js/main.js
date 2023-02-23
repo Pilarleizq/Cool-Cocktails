496b76a7509762b2029ced7088a0288e3c97c3c6
Pilarleizq <pleivaizq@gmail.com>
2023-02-23 21:39:39 +0100

    add all cocktails with fetch and event in search button

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,53 @@
-"use strict";
+'use strict';
 
-const searchButton = document.querySelector(".js-buttonSearch");
-const resetButton = document.querySelector(".js-buttonReset");
-const listCocktails = document.querySelector(".js-ulAll");
-const listFavs = document.querySelector(".js-ulFavs");
-const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita";
+const inputSearch = document.querySelector('.js-inputSearch');
+const searchButton = document.querySelector('.js-buttonSearch');
+const resetButton = document.querySelector('.js-buttonReset');
+const listCocktails = document.querySelector('.js-ulAll');
+const listFavs = document.querySelector('.js-ulFavs');
+const url =
+  'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
 
+let listMargaritaData = [];
 let listCocktailsData = [];
 
 fetch(url)
   .then((response) => response.json())
   .then((data) => {
-    console.log(data.drinks);
-    listCocktailsData = data.drinks;
-    renderListCocktails(listCocktails);
+    listMargaritaData = data.drinks;
+    renderListMargarita(listCocktails);
   });
 
-function renderListCocktails(drinks) {
-  for (const cocktail of listCocktailsData) {
+function renderListMargarita(drinks) {
+  for (const cocktail of listMargaritaData) {
     drinks.innerHTML += ` <li class="li">
-        <p> ${cocktail.strDrink} </p>
+        <p class="namelist"> ${cocktail.strDrink} </p>
         <img class="img" src=${cocktail.strDrinkThumb} alt="Foto del cocktail"/>
         </li>
         `;
   }
 }
+
+function renderListCocktails(drinks) {
+  for (const cocktail of listCocktailsData) {
+    drinks.innerHTML += ` <li class="li">
+          <p class="namelist"> ${cocktail.strDrink} </p>
+          <img class="img" src=${cocktail.strDrinkThumb} alt="Foto del cocktail"/>
+          </li>
+          `;
+  }
+}
+
+function handleClickButton() {
+  fetch(
+    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputSearch.value}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      listCocktails.innerHTML = '';
+      listCocktailsData = data.drinks;
+      renderListCocktails(listCocktails);
+    });
+}
+
+searchButton.addEventListener('click', handleClickButton);
