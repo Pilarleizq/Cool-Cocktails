984ce43fcedd55b5b5e74e1f5c196ff41a1e9470
Pilarleizq <pleivaizq@gmail.com>
2023-02-23 20:09:40 +0100

    start js

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,27 @@
-'use strict';
+"use strict";
 
+const searchButton = document.querySelector(".js-buttonSearch");
+const resetButton = document.querySelector(".js-buttonReset");
+const listCocktails = document.querySelector(".js-ulAll");
+const listFavs = document.querySelector(".js-ulFavs");
+const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita";
+
+let listCocktailsData = [];
+
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data.drinks);
+    listCocktailsData = data.drinks;
+    renderListCocktails(listCocktails);
+  });
+
+function renderListCocktails(drinks) {
+  for (const cocktail of listCocktailsData) {
+    drinks.innerHTML += ` <li class="li">
+        <p> ${cocktail.strDrink} </p>
+        <img class="img" src=${cocktail.strDrinkThumb} alt="Foto del cocktail"/>
+        </li>
+        `;
+  }
+}
